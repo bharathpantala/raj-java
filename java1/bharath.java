3575ceae57282ba2e044056ae3d8f3667e922195
chaitu <chaitu@gmail.com>
2018-05-07 20:54:54 +0530

    1.1 added some new feature

--- java1/bharath.java
+++ java1/bharath.java
@@ -4,8 +4,8 @@ public class bharath {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1;i<=1000;i++) {
-			System.out.println("rajasekhar-billa...."+i);
+		for(int i=1;i<=100;i++) {
+			System.out.println("rajasekhar..."+i);
 		}
 	}
 
