9bd27016bab76ca371235592009949adc6379e3d
chaitu <chaitu@gmail.com>
2018-05-07 20:52:45 +0530

    1.0

---
+++ java1/bharath.java
@@ -0,0 +1,12 @@
+package java1;
+
+public class bharath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=1000;i++) {
+			System.out.println("rajasekhar-billa...."+i);
+		}
+	}
+
+}
